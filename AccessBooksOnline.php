ba9dc87a828ecf9208e2d77cbccdfb0e59a28aa3
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2022-01-06 21:44:47 +0000

    A little refactoring.

--- AccessBooksOnline.php
+++ AccessBooksOnline.php
@@ -1,12 +1,9 @@
 <?php
 
-# OCDLA "Books Online" subscription product id.
-$wgBooksOnlineProductIds = array("01t0a000004Ov7LAAS");
 
 
 # Autoload classes and files
-require(__DIR__ . "/Access.php");
 
-
+$wgAutoloadClasses["AccessBooksOnline\Access"] = __DIR__ . '/Access.php';
 // Register Hooks
 $wgHooks['UserGetRights'][] = 'AccessBooksOnline\Access::onUserGetRights';
